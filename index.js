ae9521b439f6a00db7dee63acb08c23ad8bc5877
Tomas Longo <longo.tomas@googlemail.com>
2015-06-17 10:46:45 +0200

    Changed module from beeing object literal to prototype

--- index.js
+++ index.js
@@ -1,76 +1,148 @@
 var ConsoleAppender = require("./appenders/consoleappender.js"),
     util = require('util');
 
-module.exports =  {
-  regex : /%[ldm]/g,
 
-  active : true,
+var LoggerModule = module.exports = function() {
+  this.regex = /%[ldm]/g
 
-  filler : "\t",
+  this.active = true;
 
-  registeredAppenders : [],
+  this.filler = "\t";
 
-  consoleAppender : new ConsoleAppender(),
+  this.registeredAppenders = [];
 
-  messageFormat : "[%l] %d %m",
+  this.consoleAppender = new ConsoleAppender();
 
-  _format : function(message, data) {
-    return this.messageFormat.replace(this.regex, function(match) {
-      if (match == "%m") {
-        return data.message;
-      } else if (match == "%d") {
-        return new Date().toISOString();
-      } else if (match == "%l") {
-        return data.level;
-      } else {
-        return "";
-      }
-    });
-  },
-
-  info : function(message) {
-    if (this.active) {
-      var formatted = this._format(this.messageFormat, {level:"INFO", message:message});
-      if (this.registeredAppenders.length == 0) {
-        this.consoleAppender.write(formatted + "\n");
-      } else {
-        this.goThroughAppenders(formatted + "\n");
-      }
-    }
-  },
-
-  error : function(message) {
-    if (this.active) {
-      var formatted = this._format(this.messageFormat, {level:"ERROR", message:message});
-      if (this.registeredAppenders.length == 0) {
-        this.consoleAppender.write(formatted + "\n");
-      } else {
-        this.goThroughAppenders(formatted + "\n");
-      }
-    }
-  },
-
-  warning : function(message) {
-    if (this.active) {
-      var formatted = this._format(this.messageFormat, {level:"WARNING", message:message});
-      if (this.registeredAppenders.length == 0) {
-        this.consoleAppender.write(formatted + "\n");
-      } else {
-        this.goThroughAppenders(formatted + "\n");
-      }
-    }
-  },
-
-  addAppender : function(appender) {
-    this.registeredAppenders.push(appender);
-  },
-
-  goThroughAppenders : function(message) {
-    this.registeredAppenders.forEach(function(appender) {
-      appender.write(message);
-    });
-  }
+  this.messageFormat = "[%l] %d %m";
 }
+
+LoggerModule.prototype._format = function(message, data) {
+  return this.messageFormat.replace(this.regex, function(match) {
+    if (match == "%m") {
+      return data.message;
+    } else if (match == "%d") {
+      return new Date().toISOString();
+    } else if (match == "%l") {
+      return data.level;
+    } else {
+      return "";
+    }
+  });
+};
+
+LoggerModule.prototype.info = function(message) {
+  if (this.active) {
+    var formatted = this._format(this.messageFormat, {level:"INFO", message:message});
+    if (this.registeredAppenders.length == 0) {
+      this.consoleAppender.write(formatted + "\n");
+    } else {
+      this.goThroughAppenders(formatted + "\n");
+    }
+  }
+},
+
+LoggerModule.prototype.error = function(message) {
+  if (this.active) {
+    var formatted = this._format(this.messageFormat, {level:"ERROR", message:message});
+    if (this.registeredAppenders.length == 0) {
+      this.consoleAppender.write(formatted + "\n");
+    } else {
+      this.goThroughAppenders(formatted + "\n");
+    }
+  }
+},
+
+LoggerModule.prototype.warning = function(message) {
+  if (this.active) {
+    var formatted = this._format(this.messageFormat, {level:"WARNING", message:message});
+    if (this.registeredAppenders.length == 0) {
+      this.consoleAppender.write(formatted + "\n");
+    } else {
+      this.goThroughAppenders(formatted + "\n");
+    }
+  }
+},
+
+LoggerModule.prototype.addAppender = function(appender) {
+  this.registeredAppenders.push(appender);
+},
+
+LoggerModule.prototype.goThroughAppenders = function(message) {
+  this.registeredAppenders.forEach(function(appender) {
+    appender.write(message);
+  });
+}
+
+// module.exports =  {
+//   regex : /%[ldm]/g,
+//
+//   active : true,
+//
+//   filler : "\t",
+//
+//   registeredAppenders : [],
+//
+//   consoleAppender : new ConsoleAppender(),
+//
+//   messageFormat : "[%l] %d %m",
+//
+//   _format : function(message, data) {
+//     return this.messageFormat.replace(this.regex, function(match) {
+//       if (match == "%m") {
+//         return data.message;
+//       } else if (match == "%d") {
+//         return new Date().toISOString();
+//       } else if (match == "%l") {
+//         return data.level;
+//       } else {
+//         return "";
+//       }
+//     });
+//   },
+//
+//   info : function(message) {
+//     if (this.active) {
+//       var formatted = this._format(this.messageFormat, {level:"INFO", message:message});
+//       if (this.registeredAppenders.length == 0) {
+//         this.consoleAppender.write(formatted + "\n");
+//       } else {
+//         this.goThroughAppenders(formatted + "\n");
+//       }
+//     }
+//   },
+//
+//   error : function(message) {
+//     if (this.active) {
+//       var formatted = this._format(this.messageFormat, {level:"ERROR", message:message});
+//       if (this.registeredAppenders.length == 0) {
+//         this.consoleAppender.write(formatted + "\n");
+//       } else {
+//         this.goThroughAppenders(formatted + "\n");
+//       }
+//     }
+//   },
+//
+//   warning : function(message) {
+//     if (this.active) {
+//       var formatted = this._format(this.messageFormat, {level:"WARNING", message:message});
+//       if (this.registeredAppenders.length == 0) {
+//         this.consoleAppender.write(formatted + "\n");
+//       } else {
+//         this.goThroughAppenders(formatted + "\n");
+//       }
+//     }
+//   },
+//
+//   addAppender : function(appender) {
+//     this.registeredAppenders.push(appender);
+//   },
+//
+//   goThroughAppenders : function(message) {
+//     this.registeredAppenders.forEach(function(appender) {
+//       appender.write(message);
+//     });
+//   }
+// }
 
 module.exports.appenders = {
   FileAppender : require("./appenders/fileappender")
@@ -80,7 +152,7 @@ module.exports.appenders = {
 if (require.main === module) {
   var FA = new require("./appenders/fileappender.js");
 
-  var logger = module.exports;
+  var logger = new LoggerModule();
 
   // At this point no appenders have been added to the logger,
   // so it should fallback to the console
@@ -90,4 +162,11 @@ if (require.main === module) {
 
   logger.error("This should only go to file!!");
   logger.warning("And this too");
+
+  var loggerTwo = new LoggerModule();
+  loggerTwo.info("This is from the second logger and should only go to the console");
+
+  var loggerThree = new LoggerModule();
+  loggerThree.active = false;
+  loggerThree.info("This should not be printed!!!!!!!!!");
 }
--- roadmap.md
+++ roadmap.md
@@ -1,6 +1,9 @@
 ##BleedingTea
 
-
+###v0.3.0
+* **[FEATURE]** Make the logger a class, that can be instantiated.
+    * Makes it possible to create different loggers with different settings
+    for different aspects of an app.g
 
 ###v0.2.0
 * **[FEATURE]** Log format
